70f1da2c65bdd88404e2c6362cdc3a32c4674893
0z-om <ozyoom@gmail.com>
2023-07-06 20:44:53 +0100

    up

--- chat/chat.js
+++ chat/chat.js
@@ -1,15 +1,25 @@
 module.exports = (client, io) => {
   client.on("initRoom", (clientId) => {
-    console.log(clientId);
     client.join(clientId);
-    io.to(clientId).emit("yourRoomInitialized", clientId);
+    io.to(client.id).emit("room_initialized");
   });
   client.on("requestPeerID", (request) => {
-    const { senderRequest, targetRoom, receiverRequest, myPeerId: senderPeerId } = request;
-    io.to(receiverRequest).emit("receiveRequestPeer", { senderRequest, senderPeerId, targetRoom });
+    const { senderRequest, targetRoom, receiverRequest } = request;
+    io.to(receiverRequest).emit("receiveRequestPeer", { senderRequest, targetRoom });
   });
   client.on("approveRequest", (request) => {
-    const { senderRequest, roomPeersId } = request;
-    io.to(senderRequest).emit("approved", roomPeersId);
+    const { senderRequest, socketId, roomPeers, accepter } = request;
+    client.to(socketId).emit("approved", { roomPeers, accepter });
+
+    // io.sockets.sockets.get(socketId).join(accepter);
+  });
+  client.on("new_member_join", ({ receiverId, sender }) => {
+    client.to(receiverId).emit("new_member_join", sender);
+  });
+  client.on("admin_control_video_tracks", (targets) => {
+    io.to(targets.member).emit("admin_control_video_tracks", { id: targets.targetUser, controlledByAdmin: targets.controlledByAdmin });
+  });
+  client.on("admin_control_audio_tracks", (targets) => {
+    io.to(targets.member).emit("admin_control_audio_tracks", { id: targets.targetUser, controlledByAdmin: targets.controlledByAdmin });
   });
 };
