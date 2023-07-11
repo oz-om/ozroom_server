02d26486d50af262d07e1bcdcbbb5f14b0ed56c9
0z-om <ozyoom@gmail.com>
2023-07-11 18:46:43 +0100

    fix group call

--- chat/chat.js
+++ chat/chat.js
@@ -3,23 +3,33 @@ module.exports = (client, io) => {
     client.join(clientId);
     io.to(client.id).emit("room_initialized");
   });
-  client.on("requestPeerID", (request) => {
-    const { senderRequest, targetRoom, receiverRequest } = request;
-    io.to(receiverRequest).emit("receiveRequestPeer", { senderRequest, targetRoom });
+  client.on("callRequest", (request) => {
+    const { senderRequest, receiverRequest } = request;
+    io.to(receiverRequest).emit("receiveCallRequest", { senderRequest });
   });
-  client.on("approveRequest", (request) => {
-    const { senderRequest, socketId, roomPeers, accepter } = request;
-    client.to(socketId).emit("approved", { roomPeers, accepter });
 
-    // io.sockets.sockets.get(socketId).join(accepter);
+  client.on("requestIntegrationCalls", (request) => {
+    io.to(request.receiverId).emit("requestIntegrationCalls", request.senderId);
   });
-  client.on("new_member_join", ({ receiverId, sender }) => {
-    client.to(receiverId).emit("new_member_join", sender);
+
+  client.on("makeIntegrationCalls", (request) => {
+    io.to(request.receiverId).emit("makeIntegrationCalls", request.members);
   });
+
+  client.on("integrateCallRequest", (request) => {
+    io.to(request.receiverId).emit("integrateCallRequest", request.sender);
+  });
+
   client.on("admin_control_video_tracks", (targets) => {
     io.to(targets.member).emit("admin_control_video_tracks", { id: targets.targetUser, controlledByAdmin: targets.controlledByAdmin });
   });
   client.on("admin_control_audio_tracks", (targets) => {
     io.to(targets.member).emit("admin_control_audio_tracks", { id: targets.targetUser, controlledByAdmin: targets.controlledByAdmin });
+  });
+  client.on("controlled_his_video_track", (trackDetails) => {
+    io.to(trackDetails.member).emit("controlled_his_video_track", { id: trackDetails.targetUser, selfControlled: trackDetails.selfControlled });
+  });
+  client.on("controlled_his_audio_track", (trackDetails) => {
+    io.to(trackDetails.member).emit("controlled_his_audio_track", { id: trackDetails.targetUser, selfControlled: trackDetails.selfControlled });
   });
 };
